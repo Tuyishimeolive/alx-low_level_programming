78e93ece4cb7d8b95558c1e89f396be0d61d92c8
Tuyishime olive <tuyishimeolivefifi@gmail.com>
2023-07-13 07:51:17 -0700

    printef

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+  * main: entry point
+  * Description: printing as told
+  * Return: 0 sucess
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
