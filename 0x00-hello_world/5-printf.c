6ddc962f0b3d2434051b3af0ee6611a52df31519
Tuyishime olive <tuyishimeolivefifi@gmail.com>
2023-07-13 11:13:18 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-
 /**
-  * main: entry point
-  * Description: printing as told
-  * Return: 0 sucess
-  */
+* main . program that print line in printf 
+* Description: printing as told
+* Return: 0 (success)
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
