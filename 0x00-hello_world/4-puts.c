bb8fbd47e89314200efa2be7b8634fe9f51ff301
Tuyishime olive <tuyishimeolivefifi@gmail.com>
2023-07-13 10:42:17 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * main - prints "Programming is like building a multilingual puzzle
  * Return: 0 (sucess)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
